6e1dd5bcd4f62121cd662a423bd491e911c3b165
Michael Singleton <juliaks126@yahoo.com>
2023-09-03 15:08:06 -0700

    GridBuilder WIP

--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -7,18 +7,42 @@ public class TileGenerator : MonoBehaviour
 
     public string myName;
     public int myAge;
-
+    public int rowCount = 6;
+    public GameObject gridSquare;
+    public Transform placer;
 
     // Start is called before the first frame update
     void Start()
     {
-        IDMe();
+        //IDMe();
+        GridBuilder();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void PositionPlacerX()
+    {
+        placer.position += Vector3.right;
+    }
+
+    void PlaceSquare()
+    {
+        GameObject newSquare;
+        newSquare = Instantiate(gridSquare, placer.position, placer.rotation);
+        newSquare.transform.parent = transform;
+    }
+
+    void GridBuilder()
+    {
+        for (int i = 0; i < rowCount; i++)
+        {
+            PlaceSquare();
+            PositionPlacerX();
+        }
     }
 
 
