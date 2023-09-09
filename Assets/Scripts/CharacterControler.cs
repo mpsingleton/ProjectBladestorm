d002541d27ac89c4dcde3cb0d56f9907f4efef03
Michael Singleton <juliaks126@yahoo.com>
2023-09-09 11:11:25 -0700

    Basic Movement

--- Assets/Scripts/CharacterControler.cs
+++ Assets/Scripts/CharacterControler.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class CharacterControler : MonoBehaviour
 {
     public Transform character;
-
+    private float moveHorizontal;
+    private float moveVertical;
+    public float speed = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +18,19 @@ public class CharacterControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Horizontal"))
-        {
-        Debug.Log("Moving");
-        }
+        MovementValue();
+        ApplyMovement();
+    }
+
+    void MovementValue()
+    {
+        moveHorizontal = Input.GetAxis ("Horizontal") * Time.deltaTime * speed;
+        moveVertical = Input.GetAxis ("Vertical") * Time.deltaTime * speed;  
+    }
+
+    void ApplyMovement()
+    {
+        Vector2 moveValue = new Vector2(moveHorizontal, moveVertical);
+        character.Translate(moveValue);
     }
 }
